da6e6223f0905a4697e93731703997c82398c7a8
mksifat101 <mksifat101@gmail.com>
2024-01-31 09:13:24 +0600

    Single Page Are Ready

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,10 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about-us', views.about, name='about'),
+    path('programs', views.program, name='program'),
+    path('blog', views.blog, name='blog'),
+    path('event', views.event, name='event'),
+    path('contact', views.contact, name='contact'),
 ]
 
--- home/views.py
+++ home/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def program(request):
+    return render(request, 'home/program.html')
+
+
+def event(request):
+    return render(request, 'home/event.html')
+
+
+def blog(request):
+    return render(request, 'home/blog.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
